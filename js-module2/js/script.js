3709bb74d596ce4870d60b65b7669f7d0955632e
YvonneWara1 <61632053+YvonneWara1@users.noreply.github.com>
2020-06-07 17:56:09 +0200

    Add files via upload

---
+++ js-module2/js/script.js
@@ -0,0 +1,53 @@
+// Question 1
+var petArrey = ["cat", "cow", "dog"];
+
+//Question 2
+console.log(petArrey[1]);
+
+// Question 3
+
+petArrey.push("sheep");
+
+// Question 4
+
+console.log(petArrey.length);
+
+// Question 5
+
+var catObject = {
+    name: "Frank",
+    colour: "brown",
+    age: 6
+
+};
+
+// Question 6
+
+console.log(catObject.age);
+
+// Question 7
+
+var catArray = [
+    {
+        name: "Frank",
+        colour: "brown",
+        age: 6
+    
+    }
+];
+
+// Question 8
+
+for(var i = 0; i < catArray.length; i++) {
+    console.log(catArray[i]);
+}
+ 
+// Question 9
+
+function logToConsole(animal) {
+    console.log(animal);
+}
+
+// Question 10
+
+logToConsole("donkey");
